098d240823963dde3e4a681f9e98c74261e35532
nasheomirro <shanenescarimoro@gmail.com>
2023-12-20 23:58:23 +0800

    styles are now statically served

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+import path from "path";
 import express from "express";
+
 import adminRoutes from "./routes/admin.js";
 import shopRoutes from "./routes/shop.js";
 import errorHandler from "./routes/error.js";
+import { rootDir } from "./utils/path.js";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
